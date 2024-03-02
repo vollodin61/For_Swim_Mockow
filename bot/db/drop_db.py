2c2af32ebeda4c726e2974963ff7ec7785e4e987
ilia_volodin <vollodin61@gmail.com>
2024-03-02 10:04:54 +0300

    Поправил переменные в DROP и CREATE базы, теперь они импортом из настроек

--- bot/db/drop_db.py
+++ bot/db/drop_db.py
@@ -1,12 +1,13 @@
 import asyncio
-
 import asyncpg
 import psycopg2
 
+from bot.db.db_cfg import settings
 
 async def drop_database():
 	# conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="127.0.0.1")  # /// comment
-	conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="db_host", port=5432)  # /// comment  DOCKER
+	conn = psycopg2.connect(dbname=settings.DBNAME, user=settings.PGUSER, password=settings.PGPASS,
+							host=settings.PGHOSTDOCKER, port=settings.PGPORTDOCKER)  # /// comment  DOCKER
 	cursor = conn.cursor()
 
 	conn.autocommit = True
@@ -25,7 +26,8 @@ async def drop_database():
 
 def sync_create_database():
 	# conn = psycopg2.connect(dbname="postgres", user="postgres", password="123456", host="127.0.0.1")  # /// comment  ЛОкАЛ
-	conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="db_host")  # /// comment  DOCKER
+	conn = psycopg2.connect(dbname=settings.DBNAME, user=settings.PGUSER, password=settings.PGPASS,
+							host=settings.PGHOSTDOCKER, port=settings.PGPORTDOCKER)  # /// comment  DOCKER
 	# conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="localhost", port=5433)  # /// comment  DOCKER
 	cursor = conn.cursor()
 
